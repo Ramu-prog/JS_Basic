dcdfdd7279bb241e4987f896973c79bd86acf091
RamNarayan <rntestingengg@gmail.com>
2026-05-24 14:19:41 +0530

    Added ternary operator code

---
+++ src/ternaryoperator.js
@@ -0,0 +1,12 @@
+//ternary opeator wriiten as ? and :
+//if comdition is true then it wiil excute part one
+//if condition is false then it will excute part two 
+
+let num = 10;
+let bill = num >= 15 ? 100 : 200;
+console.log(bill);
+
+
+let age = 18;
+let canvote = age >= 18 ? "your eligible to vote !" : "your not we eligible to vote";
+console.log(canvote); //your are eligible to vote
